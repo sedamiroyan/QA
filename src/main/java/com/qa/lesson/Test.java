6e1dc715a282012462adcd3b150ddc699ea2ce38
sedamiroyan <seda.mir@everse-soft.com>
2022-02-09 19:54:12 +0400

    refactoring

--- src/main/java/com/qa/lesson/Test.java
+++ src/main/java/com/qa/lesson/Test.java
@@ -3,10 +3,10 @@ package com.qa.lesson;
 public class Test {
     public static void main(String[] args) {
         //Erankyan paragic
-        int ab = 8;
-        int bc = 9;
-        int ca = 11;
-        int p = ab + bc + ca;
+        int a = 8;
+        int b = 9;
+        int c = 11;
+        int p = a + b + c;
         System.out.println(p);
 // qarakusu paragic
         int qa = 3;
