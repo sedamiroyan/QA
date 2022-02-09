ec3502f7ade82c8f05d0c367fbe47850f6b442be
sedamiroyan <seda.mir@everse-soft.com>
2022-02-09 19:33:20 +0400

    first commit

---
+++ src/main/java/com/qa/lesson/Test.java
@@ -0,0 +1,12 @@
+package com.qa.lesson;
+
+public class Test {
+    public static void main(String[] args) {
+        //Erankyan paragic
+        int ab = 8;
+        int bc = 9;
+        int ca = 11;
+        int p = ab + bc + ca;
+        System.out.println(p);
+    }
+}
